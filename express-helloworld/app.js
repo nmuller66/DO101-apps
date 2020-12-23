9a9884eaadfbc21d0cbc72e910027d30273d9d46
nmuller66 <nmuller@redhat.com>
2020-12-23 10:49:31 -0700

    add /mars enpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -12,3 +12,6 @@ app.get('/mars', function(req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+});
